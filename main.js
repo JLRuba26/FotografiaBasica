14d8afe79321dc5e7afb4bdf2e81abe523fa68c2
JLRuba26 <90996138+JLRuba26@users.noreply.github.com>
2022-04-05 15:06:33 -0700

    0.3
    Actualizacion de la pagina de estilos de fotografia

--- main.js
+++ main.js
@@ -7,3 +7,26 @@ ScrollReveal().reveal('.news-cards', { delay: 500 });
 ScrollReveal().reveal('.cards-banner-one', { delay: 500 });
 ScrollReveal().reveal('.cards-banner-two', { delay: 500 });
 
+
+//Scroll View
+document.getElementById("botonarriba").addEventListener("click", scrollUp);
+
+function scrollUp(){
+  var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+  if(currentScroll > 0){
+    window.requestAnimationFrame(scrollUp);
+    window.scrollTo (0, currentScroll - (currentScroll / 5));
+    buttonUp.style.transform = "scale(0)"
+  }
+}
+
+buttonUp = document.getElementById("botonarriba");
+
+window.onscroll = function(){
+  var scroll = document.documentElement.scrollTop;
+  if(scroll > 300){
+    buttonUp.style.transform = "scale(1)"
+  }else if(scroll < 300){
+    buttonUp.style.transform = "scale(0)"
+  }
+}
